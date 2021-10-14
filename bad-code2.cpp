e2fb1f652578ae1180a52ee33cb7fb5000f1a218
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2021-10-14 12:26:26 -0400

    added a second example of bad code

---
+++ bad-code2.cpp
@@ -0,0 +1,14 @@
+                            int main(){
+                    std::string test = "This is for a test";
+  if (test == "This is for a test"){
+              for( int i = 0; i <10; i++){
+      std::cout << i << '\n';
+                                        if(i%2 == 0){
+        std::cout << "even" << '\n';
+                                              }
+      else{
+                  std::cout << "odd" << '\n';
+      }
+                            }
+                  }
+                                      }
